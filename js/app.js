62e845b35c11d84d813f93c4efdf2fe0482963c6
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-01-01 16:49:45 +0600

    steps added

--- js/app.js
+++ js/app.js
@@ -2,9 +2,19 @@ const header = document.getElementById("header");
 
 // make header postion fixed when scroll down
 window.addEventListener("scroll", () => {
-  header.classList.toggle("fixed", window.scrollY > 0);
+  header.classList.toggle("fixed", window.scrollY > 200);
 });
-
 
 const year = document.getElementById("year");
 year.innerHTML = new Date().getFullYear();
+
+$(".fade-slider").slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  arrows: false,
+  cssEase: "linear",
+  autoplay: true,
+  autoplaySpeed: 4000,
+});
